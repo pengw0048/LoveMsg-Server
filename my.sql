edb175233d6b5fd40a5f3c138846fa64e3fea816
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2016-06-23 20:46:41 +0800

    Set database structure.

---
+++ my.sql
@@ -0,0 +1,84 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.9-MariaDB : Database - lovemsg
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lovemsg` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `lovemsg`;
+
+/*Table structure for table `groupinfo` */
+
+DROP TABLE IF EXISTS `groupinfo`;
+
+CREATE TABLE `groupinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `startdate` varchar(25) DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `groupinfo` */
+
+/*Table structure for table `groupmember` */
+
+DROP TABLE IF EXISTS `groupmember`;
+
+CREATE TABLE `groupmember` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupid` int(11) DEFAULT NULL,
+  `memberid` int(11) DEFAULT NULL,
+  `membername` varchar(20) DEFAULT NULL,
+  `lastlogin` datetime DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `groupmember` */
+
+/*Table structure for table `message` */
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupid` int(11) DEFAULT NULL,
+  `senderid` int(11) DEFAULT NULL,
+  `content` varchar(300) DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `message` */
+
+/*Table structure for table `messagestat` */
+
+DROP TABLE IF EXISTS `messagestat`;
+
+CREATE TABLE `messagestat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupid` int(11) DEFAULT NULL,
+  `memberid` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `messagestat` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
